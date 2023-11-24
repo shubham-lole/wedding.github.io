afaafe33b9ccc2182c45afd8a8ba14db527e4e33
Shubham Lole <70950860+shubham-lole@users.noreply.github.com>
2023-11-24 17:11:47 +0530

    add files

---
+++ script.js
@@ -0,0 +1,19 @@
+const carouselItems = document.querySelectorAll(".carousel_item"); 
+let i = 1;
+
+setInterval(() => {
+ Array.from(carouselItems).forEach((item,index) => {
+
+   if(i < carouselItems.length){
+    item.style.transform = `translateX(-${i*100}%)`
+   }
+  })
+
+
+  if(i < carouselItems.length){
+    i++;
+  }
+  else{
+    i=0;
+  }
+},2000)
